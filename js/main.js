86180fc1eb8f564377b4778ed1f0de6cb87ea1fd
AlexsandroD <adrdra88@gmail.com>
2021-12-07 17:36:27 +0100

    adding bits

--- js/main.js
+++ js/main.js
@@ -2,25 +2,26 @@ const app = new Vue({
     el: "#root",
     data: {
 
-        contacts: [{
+        contacts: [
+            {
                 name: 'Michele',
                 avatar: '_1',
                 visible: true,
                 messages: [{
-                        date: '10/01/2020 15:30:55',
-                        message: 'Hai portato a spasso il cane?',
+                        date: '28/03/2020 10:10 AM',
+                        message: 'Mario',
+                        status: 'received',
+                        menu: false
+                    },
+                    {
+                        date: '28/03/2020 10:20 AM',
+                        message: 'Gaetano?',
                         status: 'sent',
                         menu: false
                     },
                     {
-                        date: '10/01/2020 15:50:00',
-                        message: 'Ricordati di dargli da mangiare',
-                        status: 'sent',
-                        menu: false
-                    },
-                    {
-                        date: '10/01/2020 16:15:22',
-                        message: 'Tutto fatto!',
+                        date: '28/03/2020 16:15 PM',
+                        message: 'Ah scusa!',
                         status: 'received',
                         menu: false
                     }
@@ -31,19 +32,19 @@ const app = new Vue({
                 avatar: '_2',
                 visible: true,
                 messages: [{
-                        date: '20/03/2020 16:30:00',
+                        date: '20/03/2020 16:30 PM',
                         message: 'Ciao come stai?',
                         status: 'sent',
                         menu: false
                     },
                     {
-                        date: '20/03/2020 16:30:55',
+                        date: '20/03/2020 16:30 PM',
                         message: 'Bene grazie! Stasera ci vediamo?',
                         status: 'received',
                         menu: false
                     },
                     {
-                        date: '20/03/2020 16:35:00',
+                        date: '20/03/2020 16:35 PM',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                         status: 'received',
                         menu: false
@@ -55,72 +56,74 @@ const app = new Vue({
                 avatar: '_3',
                 visible: true,
                 messages: [{
-                        date: '28/03/2020 10:10:40',
+                        date: '28/03/2020 10:10 AM',
                         message: 'La Marianna va in campagna',
                         status: 'received',
                         menu: false
                     },
                     {
-                        date: '28/03/2020 10:20:10',
+                        date: '28/03/2020 10:20 AM',
                         message: 'Sicuro di non aver sbagliato chat?',
                         status: 'sent',
                         menu: false
                     },
                     {
-                        date: '28/03/2020 16:15:22',
+                        date: dayjs().format('28/03/2020 16:15 A'),
                         message: 'Ah scusa!',
                         status: 'received',
                         menu: false
                     }
                 ],
             },
+
             {
                 name: 'Monica',
                 avatar: '_io',
                 visible: true,
                 messages: [{
-                        date: '10/01/2020 15:30:55',
-                        message: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent',
-                        menu:false
-                    },
-                    {
-                        date: '10/01/2020 15:50:00',
-                        message: 'fanno i croquette buoni?',
+                        date: dayjs().format('28/03/2020 10:10 A'),
+                        message: 'Bi voglio una papera',
                         status: 'received',
                         menu: false
                     },
                     {
-                        date: '10/01/2020 15:50:00',
-                        message: 'sicuramente no peró proviamo',
+                        date: dayjs().format('28/03/2020 10:20 A'),
+                        message: 'Te la compro per natale',
                         status: 'sent',
+                        menu: false
+                    },
+                    {
+                        date: dayjs().format('28/03/2020 10:22 A'),
+                        message: 'siiiiii',
+                        status: 'received',
                         menu: false
                     }
                 ],
             },
+           
         ],
 
 
      
         currentUser: 0,
-        lastMessage:null,
+        
         sent: 'sent',
         received: "received",
         messageContent: null,
-        activeChat: 'activeChat',
+        activeChat: '',
         searchUtente: '',
         none: 'none',
         lastAccess: 'Ultimo acesso', 
         transform: 'transform',
         rotate:'rotate',
-        microfono:false
-            
+        microfono:false,
+        intro:true,
+        blue:'blue'
+       
+
+    
         
 
-    },
-
-    mounted: function () {
-        this.lastSeen();
     },
 
 
@@ -128,39 +131,31 @@ const app = new Vue({
 
     methods: {
         active: function (index) {
-            this.currentUser = index;
- 
+                this.currentUser = index;
         },
-
-
         addMessage: function (current,index) {
             if (this.messageContent != '') {
-
-                
-
                 this.contacts[current].messages.push({
                     message: this.messageContent,
                     status: 'sent',
                     menu: false,
-                    date: dayjs().format('DD/MM/YYYY,HH,mm,ss'),
+                    date: dayjs().format('DD/MM/YYYY HH:mm A'),
                 }),
-              
                this.answer()
-               
-                
             }
             this.messageContent = '';
         },
+
         addAnswer: function (index) {
             console.log()
             this.contacts[this.currentUser].messages.push({
                 message: 'ok',
                 status: 'received',
                 menu: false,
-                date: dayjs().format('DD/MM/YYYY,HH,mm,ss'),
+                date: dayjs().format('DD/MM/YYYY HH:mm A'),
             })
-            this.lastAccess = 'Ultimo accesso ' + dayjs().format('DD/MM/YYYY HH:mmss')
-            
+            this.lastAccess = 'Ultimo accesso ' + dayjs().format('DD/MM/YYYY HH:mm A') 
+            this.myFunction()
         },
 
         answer: function () {
@@ -169,34 +164,20 @@ const app = new Vue({
             }, 1000);
         },
 
-
-
         filter: function () {
             if (this.searchUtente != '') {
 
                 this.contacts.forEach(element => {
                     if (element.name.toLowerCase().includes(this.searchUtente.toLowerCase())) {
                         element.visible = true;
-                    } else {
-                        element.visible = false;
+                    } else if (element.name.toLowerCase().includes(this.searchUtente == '')) {
+                        element.visible = true;
+                    }else{
+                        element.visible = false
                     }
-
-
                 });
             }
         },
-
-
-        lastSeen: function (index) {
-            this.contacts[this.currentUser].messages.forEach(element => {
-                if (element.status == 'received') {
-                    this.lastAccess = 'last seen' + ' ' + element.date
-                }
-            });
-        },
-
-
-        
 
         dropDown:function(index){
             
@@ -212,23 +193,39 @@ const app = new Vue({
                 this.contacts.messages.menu = false
         }},
 
-       
-
         deleteMsg: function(current,index){
           this.contacts[current].messages.splice(index,1)
         },
 
+        mic:function(){
+            if(this.messageContent != 0){
+                this.microfono = true
 
-       mic:function(){
-           if(this.messageContent != 0){
-               this.microfono = true
+            }else{
+                this.microfono = false
+            }
+            
+        },
 
-           }else{
-               this.microfono = false
-           }
-           
-       }
+        introPage:function(){
+            if(this.intro == true){
+                this.intro = false
+                this.activeChat = 'active-chat'
+            }
+        },
+
+       lastMessage: function (index) {
+           const last = this.contacts[index].messages.length - 1;
+           return this.contacts[index].messages[last].message;
+       },
+
+         lastDate: function (index) {
+             const last = this.contacts[index].messages.length - 1;
+             return this.contacts[index].messages[last].date;
+         }
+                           
        
+        
     },
 
 
